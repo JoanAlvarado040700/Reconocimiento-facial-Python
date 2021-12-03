fac19bbb94d9f7c40fe6ad1aaa6e3d230c3c3334
JoanAlvarado040700 <sevaslarios@gmail.com>
2021-12-02 23:41:54 -0600

    Lo basico

--- capaEntrada.py
+++ capaEntrada.py
@@ -1,5 +1,21 @@
 import cv2 as cv
-
+import numpy as np
 ruidos = cv.CascadeClassifier('D:\Proyectos python\Entrenamiento de ruidos opencv\opencv-4.x\data\haarcascades\haarcascade_frontalface_default.xml')
 
-print(ruidos)
+camara = cv.VideoCapture(0) #es para abrir la camara 
+
+while True:
+    _,captura = camara.read()
+    grises = cv.cvtColor(captura,cv.COLOR_BGR2GRAY)
+    cara = ruidos.detectMultiScale(grises,1.2,5)
+    for (x,y,e1,e2) in cara:
+        cv.rectangle(captura,(x,y),(x+e1,y+e2),(0, 0, 255),2)
+    
+    cv.imshow("Resultados", captura)
+
+    if cv.waitKey(1) == ord('s'):
+        break
+camara.release()
+cv.destroyAllWindows()
+
+
