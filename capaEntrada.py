a9fc085014e8425ab88c37137dc85f9eb4622852
JoanAlvarado040700 <sevaslarios@gmail.com>
2021-12-02 23:19:38 -0600

    Lo basico

---
+++ capaEntrada.py
@@ -0,0 +1,5 @@
+import cv2 as cv
+
+ruidos = cv.CascadeClassifier('D:\Proyectos python\Entrenamiento de ruidos opencv\opencv-4.x\data\haarcascades\haarcascade_frontalface_default.xml')
+
+print(ruidos)
